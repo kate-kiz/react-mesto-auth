802645a54d709fddb255763ba97e2f470770d962
Ekaterina Kizikova <kizikovaek@yandex.ru>
2023-06-16 21:36:35 +0700

    ПР12

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,78 +1,83 @@
 
-// import { useState } from "react";
-
-// function Login({ onLogin }) {
-
-//     const [formValues, setFormValues] = useState({ email: '', password: '' })
-
-//     function handleChange(evt) {
-//         const { name, value } = evt.target;
-//         setFormValues({
-//             ...formValues,
-//             [name]: value
-//         })
-//     }
-
-//     function handleSubmit(evt) {
-//         evt.preventDefault();
-//         const { email, password } = formValues;
-//         onLogin(email, password);
-//     }
-
-//     return (
-//         <div className='login entry-page'>
-
-//             <h1 className='entry-page__title'>Вход</h1>
-//             <form
-//                 className='entry-page__form entry-page__form_login'
-//                 onSubmit={handleSubmit}
-//             >
-//                 <input
-//                     type="email"
-//                     name='email'
-//                     value={formValue.email}
-//                     onChange={handleChange}
-//                     className='entry-page__input entry-page__input_email'
-//                     placeholder='Email'
-//                     required
-//                 />
-//                 <input
-//                     type="password"
-//                     name="password"
-//                     value={formValue.password}
-//                     onChange={handleChange}
-//                     className='entry-page__input entry-page__input_password'
-//                     placeholder='Пароль'
-//                     required
-//                 />
-//                 <button
-//                     disabled={onDisabled}
-//                     type="submit"
-//                     className='entry-page__submit'>
-//                     Войти
-//                 </button>
-//             </form>
-
-
-//         </div>
-//     );
-
-// }
-
-// export default Login;
-
-import AuthorizationPopup from "./AuthorizationPopup";
+import { useState } from "react";
 
 function Login({ onLogin }) {
+
+    const [formValues, setFormValues] = useState({ email: '', password: '' })
+
+    function handleChange(evt) {
+        const { name, value } = evt.target;
+        setFormValues({
+            ...formValues,
+            [name]: value
+        })
+    }
+
+    function handleSubmit(evt) {
+        evt.preventDefault();
+        const { email, password } = formValues;
+        onLogin(email, password);
+    }
+
     return (
-        <AuthorizationPopup
-            title='Вход'
-            buttonText='Войти'
-            linkText='Нет аккаунта? Создать'
-            linkPath='/sign-up'
-            onSubmit={onLogin}
-        />
-    )
+        <div className='auth'>
+
+            <h1 className='auth__title'>Вход</h1>
+            <form
+                className='auth__form auth__form_login'
+                onSubmit={handleSubmit}
+            >
+                <div className="auth__section">
+                    <input
+                        type="email"
+                        name='email'
+                        value={formValues.email}
+                        onChange={handleChange}
+                        className='auth__input'
+                        placeholder='E-mail'
+                        required
+                    />
+                    <span className="auth__error" id="input-error-email" />
+                </div>
+                <div className="auth__section">
+                    <input
+                        type="password"
+                        name="password"
+                        value={formValues.password}
+                        onChange={handleChange}
+                        className='auth__input'
+                        placeholder='Пароль'
+                        required
+                    />
+                    <span className="auth__error" id="input-error-password" />
+                </div>
+                <button
+                    type="submit"
+                    className='auth__button'>
+                    Войти
+                </button>
+            </form>
+
+
+        </div>
+    );
+
 }
 
 export default Login;
+
+// import AuthorizationPopup from "./AuthorizationPopup";
+
+// function Login({ onLogin }) {
+//     return (
+//         <AuthorizationPopup
+//             title='Вход'
+//             buttonText='Войти'
+//             linkText='Нет аккаунта? Создать'
+//             linkPath='/sign-up'
+//             onSubmit={onLogin}
+//         />
+//     )
+// }
+
+// export default Login;
